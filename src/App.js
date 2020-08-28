4da5372c19c5099279a0b1d91282bd9382edacbc
Diego Andrade <andrade.diego@outlook.com>
2020-08-28 10:05:11 -0400

    added loops and if statement

--- src/App.js
+++ src/App.js
@@ -8,59 +8,51 @@ class App extends Component {
   state = {
     persons: [
       {
+        id: 1,
         name: 'Diego',
         age: 35
       },
       {
+        id: 2,
         name: 'Josy',
         age: 36
       },
       {
+        id: 3,
         name: 'Dave',
         age: 30
       }
     ],
-    hobbie: 'My hobbie is baseball'
+    hobbie: 'My hobbie is baseball',
+    showPersons: false
   }
 
-  switchNameHandler = (newName) => {
+  nameChangeHandler = (event, personId) => {
     // DO NOT DO THIS
     // this.state.persons[0].name = 'Jane';
-    this.setState({
-      persons: [
-        {
-          name: newName,
-          age: 35
-        },
-        {
-          name: 'Josy',
-          age: 25
-        },
-        {
-          name: 'Joe',
-          age: 30
-        }
-      ]
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === personId
     });
+
+    const person = {...this.state.persons[personIndex]};
+    person.name = event.target.value;
+    const persons = [...this.state.persons]
+
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
   }
 
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
 
-  nameChangeHandler = (event) => {
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons
     this.setState({
-      persons: [
-        {
-          name: 'Andrade',
-          age: 35
-        },
-        {
-          name: 'Josy',
-          age: 25
-        },
-        {
-          name: event.target.value,
-          age: 30
-        }
-      ]
+      showPersons: !doesShow
     });
   }
 
@@ -73,33 +65,45 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {
+            this.state.persons.map((person, index) => {
+              return (              
+                <Person 
+                  click={() => this.deletePersonHandler(index)}                  
+                  name={person.name} 
+                  age={person.age}
+                  key={person.id}
+                  changed={ (event) => this.nameChangeHandler(event, person.id)}
+                  >                    
+                </Person>              
+              )
+            })
+          }
+          
+        </div> 
+      );
+    }
+
     return (
       <div>        
         <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <Person name={this.state.persons[0].name} 
-               age={this.state.persons[0].age}/>
-            </div>
-            <div className="col-md-4">
-              <Person name={this.state.persons[1].name} 
-                click={this.switchNameHandler.bind(this, 'Josiane')}
-                changed={this.nameChangeHandler}
-                age={this.state.persons[1].age}/>
-            </div>
-            <div className="col-md-4">
-              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
-                {this.state.hobbie}
-              </Person>
-            </div>
-          </div>        
-          <div className="row">
+        <div className="row">
             <button
               style={style} 
-              onClick={() => this.switchNameHandler('Rogerio')}>
-                Switch Name
+              onClick={this.togglePersonsHandler}>
+                Toggle Persons
             </button>
           </div>
+          <div className="row">
+            <div className="col-md-12">
+              {persons}
+            </div>
+          </div>                  
         </div>
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,7 +6,7 @@ const person = (props) => {
 		<div className="Person">
 			<h4 onClick={props.click}>I'm a {props.name} and I am {props.age} years old</h4>
 			<h5>{props.children}</h5>
-			<input type="text" onChange={props.changed} />
+			<input type="text" onChange={props.changed} value={props.name} />
 		</div>
 	)
 };
